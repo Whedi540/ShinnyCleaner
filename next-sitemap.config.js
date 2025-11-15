fa458b7d9a228ef3bb75c74bd1131c985455826f
Waleed Hassan <138555889+Whedi540@users.noreply.github.com>
2025-11-15 22:27:39 +0500

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.shinnycleaners.vercel.app', 
+  siteUrl: 'https://www.shinnycleaner.com', 
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
